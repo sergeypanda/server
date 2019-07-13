6157ddee59fe13eaf41e692f2fdb4be8078c43c7
sergeypanda <sergeypanda@gmail.com>
2019-07-12 22:15:48 -0400

    new file:   client/index.html 	new file:   client/js/send.js 	README_ME 	libs/

---
+++ client/js/send.js
@@ -0,0 +1,30 @@
+function SendRequestPOST(url, data)
+{
+	console.log('here');
+	var xhttp = new XMLHttpRequest();
+	xhttp.open('POST', url)
+	xhttp.onreadystatechange = function(){
+		if(this.readyState ==4 && this.status == 200){
+			console.log(xhttp.responseText);		
+		}
+	}
+	xhttp.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+	xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhttp.send(data);
+	return xhttp;
+}
+
+function SendRequestGET(url, success)
+{
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function(){
+		if(this.readyState > 3 && this.status == 200)
+		{
+			success(xhttp.responseText);
+		}
+	}
+
+	xhttp.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+	xhttp.send();
+	return xhttp;
+}
